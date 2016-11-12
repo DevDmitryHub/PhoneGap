79ca4c323dd1b0ff713b659b40100a98a104eba7
Maxim Shoustin <maxim@appsflyer.com>
2016-11-12 22:53:12 +0200

    comes from “platforms”

---
+++ src/ios/AppsFlyerPlugin.h
@@ -0,0 +1,23 @@
+#import <Foundation/Foundation.h>
+#import <Cordova/CDVPlugin.h>
+#import "AppsFlyerTracker.h"
+
+
+@interface AppsFlyerPlugin : CDVPlugin <UIApplicationDelegate, AppsFlyerTrackerDelegate>
+- (void)setCurrencyCode:(CDVInvokedUrlCommand*)command;
+- (void)setAppUserId:(CDVInvokedUrlCommand*)command;
+- (void)getAppsFlyerUID:(CDVInvokedUrlCommand*)command;
+- (void)sendTrackingWithEvent:(CDVInvokedUrlCommand*)command;
+- (void)onConversionDataReceived:(NSDictionary*) installData;
+- (void)onConversionDataRequestFailure:(NSError *) error;
+- (void)trackEvent:(CDVInvokedUrlCommand*)command;
+- (void)registerUninstall:(CDVInvokedUrlCommand*)command;
+@end
+
+
+
+
+// Appsflyer JS objects
+#define afDevKey @"devKey"
+#define afAppId @"appId"
+#define afIsDebug @"isDebug"
